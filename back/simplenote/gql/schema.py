c22663500833c4ce7184b8718401225c60eab4d3
Timur Kapitonov <tkapitonov@pandarium.pro>
2020-08-22 14:06:31 +0300

    first commit with backend logic and mutations

---
+++ back/simplenote/gql/schema.py
@@ -0,0 +1,25 @@
+from graphene import Argument, Field, ID, ObjectType, Schema
+from .filters import NoteFilter
+from notes.models import Note
+from .types import NoteType
+from graphene_django.filter import DjangoFilterConnectionField
+from .mutations import NoteDelete, NoteCreate
+
+
+class Query(ObjectType):
+    notes = DjangoFilterConnectionField(NoteType, filterset_class=NoteFilter)
+    note = Field(NoteType, id=Argument(ID, required=True))
+
+    def resolve_notes(self, info, **kwargs):
+        return Note.objects.all()
+
+    def resolve_note(self, info, **kwargs):
+        return Note.objects.filter(pk=kwargs.get("id")).last()
+
+
+class Mutation(ObjectType):
+    note_create = NoteCreate.Field()
+    note_delete = NoteDelete.Field()
+
+
+schema = Schema(query=Query, mutation=Mutation)
